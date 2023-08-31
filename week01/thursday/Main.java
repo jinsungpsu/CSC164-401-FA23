d9d14f02948bfa17b468fee7eaca4de192f2d1e4
Jin An <jin.an@dtcc.edu>
2023-08-31 10:36:26 -0400

    Create Main.java

---
+++ week01/thursday/Main.java
@@ -0,0 +1,31 @@
+/*
+Jin An
+DTCC Lab 1
+8/31/21
+
+I wrote all this code or provided citations where needed.  I used IntelliJ to generate some of the code.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Hi");
+        System.out.print("Bye");
+
+        System.out.println("Hello");
+        // same as doing System.out.print("Hello\n");
+
+
+        int age = 99;
+        String name = "John";
+        name += " Smith";
+        name += " age is" + 99;
+
+        System.out.println(name);
+
+        System.out.print("Hello and welcome!");
+        // cout << "Hello" << endl << "World!";
+        System.out.println("Hello" + " \n my name is " + " Mr. An");
+        System.out.println("my age is: " + age);
+
+    }
+}
