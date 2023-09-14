978b4b0005e4f4d26461797bdf9f5f0ae9bd665a
Jin An <jin.an@dtcc.edu>
2023-09-14 08:25:02 -0400

    Create ArraysReview.java

---
+++ week03/thursday/ArraysReview.java
@@ -0,0 +1,22 @@
+public class ArraysReview {
+    public static void main(String[] args) {
+        /*
+            Print out to the screen
+            (OK to hardcode, but use loop if you can)
+            Exercise 0: all letters
+            Exercise 1: a b c
+            Exercise 2: d e f
+            Exercise 3: a d g
+            Exercise 4: c f i
+            Exercise 5: a e i
+         */
+        char[][] arr = {
+                {'a', 'b', 'c'},
+                {'d', 'e', 'f'},
+                {'g', 'h', 'i'}
+        };
+
+    }
+
+}
+
